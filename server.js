ea70dbeb26202ccc482acdbbae8233c9cb68051c
chabger10553 <moinsejdija99@gmail.com>
2026-02-23 14:17:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 8080;
 
+app.use(express.json());
+
+// Healthcheck für Railway
 app.get('/', (req, res) => {
   res.send('SERVER LÄUFT!');
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log('Server läuft auf Port ' + PORT);
-=======
-const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-  res.send('SERVER LÄUFT!');
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
 });
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log('Server läuft auf Port ' + PORT);
->>>>>>> 39677f0b0fbef1207a78de5bf5c9eae9d71fd8a8
 });
